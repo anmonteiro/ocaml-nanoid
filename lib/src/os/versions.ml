620bb69410774bc8d3285ae2fe9b5534b3c96688
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2023-09-15 21:15:57 -0700

    use mirage crypto instead of cryptokit

--- lib/src/os/versions.ml
+++ lib/src/os/versions.ml
@@ -3,7 +3,7 @@ module Intf = struct
   module type RNG = sig
     (** [random_bytes size] returns a buffer and a offset at which [size] fresh
         random bytes are readable. *)
-    val random_bytes : int -> bytes * int
+    val random_bytes : int -> bytes
   end
 
   (** Nano ID generator *)
@@ -17,14 +17,6 @@ end
 module type VERSION_1_0 = sig
   (** @inline *)
   include module type of Intf
-
-  (** [pseudo_seeded seed] is a [RNG] module that generates pseudo random
-      numbers based on the given [seed]. It is subject to the same limitations
-      as {!Cryptokit.Random.pseudo_rng} *)
-  val pseudo_seeded : string -> (module RNG)
-
-  (** [Make (Rng)] is a Nano ID generator based on random number generator [Rng] *)
-  module Make (Rng : RNG) : S
 
   (** [Simple ()] is a Nano ID generator based on the {!pseudo_seeded} random
       number generator seeded with the current system time as given by
--- tests/os.ml
+++ tests/os.ml
@@ -1,4 +1,5 @@
 let () =
+  Mirage_crypto_rng_unix.initialize (module Mirage_crypto_rng.Fortuna);
   let open Alcotest in
   run "Nanoid"
     [ ("pseudo-seeded", [ test_case "generate" `Quick Tests.generate ]) ]
