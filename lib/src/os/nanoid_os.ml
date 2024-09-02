fb0f4f3262d33d537c9465262264c7d9b5621ee0
Antonio Nuno Monteiro <anmonteiro@gmail.com>
2024-09-02 16:11:42 +0200

    sans cstruct

--- lib/src/os/nanoid_os.ml
+++ lib/src/os/nanoid_os.ml
@@ -5,9 +5,9 @@ let alphabet =
 
 
 module Simple () = struct
-  let random_bytes n = Cstruct.to_bytes (Mirage_crypto_rng.generate n)
+  let random_string n = Mirage_crypto_rng.generate n
   let nanoid ?(size = 21) () =
-    let bytes = random_bytes size in
+    let bytes = random_string size in
     String.init size (fun i ->
-        alphabet.[Bytes.get_uint8 bytes i land 0b00111111])
+        alphabet.[String.get_uint8 bytes i land 0b00111111])
 end
